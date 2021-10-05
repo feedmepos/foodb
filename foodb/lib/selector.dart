7abbe5e65e8a6d2478a03a4ea161b71f875e3d93
foofoofoo <fooziqin@gmail.com>
2021-10-05 11:59:38 +0800

    selector

--- foodb/lib/selector.dart
+++ foodb/lib/selector.dart
@@ -1,4 +1,6 @@
 //for _index and _find func
+import 'package:collection/equality.dart';
+
 abstract class Operator {
   late String operator;
   bool evaluate();
@@ -12,12 +14,208 @@ class EqualOperator extends ConditionOperator {
 
   @override
   bool evaluate() {
-    if (value is String && expected is String) {
-      return value.compareTo(expected) == 0;
+    try {
+      if (value is List || value is Map) {
+        return DeepCollectionEquality().equals(value, expected);
+      }
+      return value == expected;
+    } catch (e) {
+      return false;
     }
-    return true;
   }
 }
+
+class NotEqualOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    try {
+      if (value is List || value is Map) {
+        return !DeepCollectionEquality().equals(value, expected);
+      }
+      return value != expected;
+    } catch (e) {
+      return true;
+    }
+  }
+}
+
+class GreaterThanOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is String && expected is String) {
+      return value.compareTo(expected) > 0;
+    } else if (value is int && expected is int) {
+      return value.compareTo(expected) > 0;
+    } else if (value is String && expected is int) {
+      return true;
+    } else if (value is int && expected is String) {
+      return false;
+    }
+    return false;
+  }
+}
+
+class GreaterThanOrEqualOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is String && expected is String) {
+      return value.compareTo(expected) >= 0;
+    } else if (value is int && expected is int) {
+      return value.compareTo(expected) >= 0;
+    } else if (value is String && expected is int) {
+      return true;
+    } else if (value is int && expected is String) {
+      return false;
+    }
+    return false;
+  }
+}
+
+class LessThanOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is String && expected is String) {
+      return value.compareTo(expected) < 0;
+    } else if (value is int && expected is int) {
+      return value.compareTo(expected) < 0;
+    } else if (value is String && expected is int) {
+      return false;
+    } else if (value is int && expected is String) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class LessThanOrEqualOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is String && expected is String) {
+      return value.compareTo(expected) <= 0;
+    } else if (value is int && expected is int) {
+      return value.compareTo(expected) <= 0;
+    } else if (value is String && expected is int) {
+      return false;
+    } else if (value is int && expected is String) {
+      return true;
+    }
+    return false;
+  }
+}
+
+class ExistsOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    return (value != null) == expected;
+  }
+}
+
+class TypeOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is String) {
+      return expected == "string";
+    } else if (value is num) {
+      return expected == "number";
+    } else if (value is bool) {
+      return expected == "boolean";
+    } else if (value is List) {
+      return expected == "array";
+    } else if (value is Map) {
+      return expected == "object";
+    } else if (value == null) {
+      return expected == "null";
+    }
+    return false;
+  }
+}
+
+class InOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (expected is List) {
+      return expected.contains(value);
+    }
+    return false;
+  }
+}
+
+class NotInOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (expected is List) {
+      return !expected.contains(value);
+    }
+    return false;
+  }
+}
+
+class SizeOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is List) {
+      return value.length==expected;
+    }
+    return false;
+  }
+}
+
+class ModOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is int && expected is List && expected.length==2) {
+      return value%expected[0]==expected[1];
+    }
+    return false;
+  }
+}
+
+class RegexOperator extends ConditionOperator {
+  dynamic value;
+  dynamic expected;
+
+  @override
+  bool evaluate() {
+    if (value is String && expected is String) {
+      return RegExp("${expected}\$").hasMatch(value);
+    }
+    return false;
+  }
+}
+
 
 abstract class CombinationOperator extends Operator {
   List<Operator> operators = [];
@@ -37,6 +235,13 @@ class AndOperator extends CombinationOperator {
   @override
   bool combine(bool a, bool b) {
     return a && b;
+  }
+}
+
+class OrOperator extends CombinationOperator {
+  @override
+  bool combine(bool a, bool b) {
+    return a || b;
   }
 }
 
--- foodb/test/selector_test.dart
+++ foodb/test/selector_test.dart
@@ -2,15 +2,29 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:foodb/selector.dart';
 
 void main() {
-  test('combination equal', () {
+  test('and, equal', () {
     final and = AndOperator();
     final equal1 = EqualOperator();
     equal1.value = 'a';
     equal1.expected = 'a';
+    
     final equal2 = EqualOperator();
-    equal2.value = 'a';
-    equal2.expected = 'b';
+    equal2.value = 1;
+    equal2.expected = 1;
     and.operators.addAll([equal1, equal2]);
     expect(and.evaluate(), true);
+  });
+
+  test('or, not equal', () {
+    final or = OrOperator();
+    final equal1 = NotEqualOperator();
+    equal1.value = 'a';
+    equal1.expected = 'a';
+    
+    final equal2 = NotEqualOperator();
+    equal2.value = 'a';
+    equal2.expected = 'b';
+    or.operators.addAll([equal1, equal2]);
+    expect(or.evaluate(), true);
   });
 }
