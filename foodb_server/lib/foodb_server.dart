5bfb34c49d781c143b7150d551fb2919e113316e
HongJian <hongjian@feedme.cc>
2022-09-07 11:15:49 +0800

    fixed typing

--- foodb_server/lib/foodb_server.dart
+++ foodb_server/lib/foodb_server.dart
@@ -1,6 +1,7 @@
 import 'package:foodb/foodb.dart';
 import 'package:shelf_web_socket/shelf_web_socket.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 class Request {
   String path;
@@ -21,16 +22,29 @@ abstract class FoodbServer {
   final Foodb db;
   FoodbServer(this.db);
 
-  Future<dynamic> get(Request request);
+  Future<dynamic> _get(Request request) async {
+    return db.get(id: request.path, fromJsonT: (v) => v);
+  }
+
+  Future<Map<String, dynamic>> _handleRequest(Request request) async {
+    switch (request.path) {
+      case 'get':
+        {
+          // /restaurant_123/asdasd
+          return await _get(request);
+        }
+      default:
+        throw Exception('invalid ${request.path}');
+    }
+  }
 }
 
-class WebSocketFoodbServer implements FoodbServer {
-  @override
-  final Foodb db;
-  WebSocketFoodbServer(this.db) {
-    final handler = webSocketHandler((websocket) {
+class WebSocketFoodbServer extends FoodbServer {
+  WebSocketFoodbServer(Foodb db) : super(db) {
+    final handler = webSocketHandler((WebSocketChannel websocket) {
       websocket.stream.listen((message) async {
-        final result = await _handleRequest(message);
+        print("echo $message");
+        final result = await _handleRequest(Request.fromJson(message));
         websocket.sink.add({...result, 'messageId': message['messageId']});
       });
     });
@@ -38,25 +52,6 @@ class WebSocketFoodbServer implements FoodbServer {
     shelf_io.serve(handler, 'localhost', 6984).then((server) {
       print('Serving at ws://${server.address.host}:${server.port}');
     });
-  }
-
-  _handleRequest(Map<String, dynamic> message) async {
-    print("echo $message");
-    final request = Request.fromJson(message);
-    switch (request.path) {
-      case 'get':
-        {
-          // /restaurant_123/asdasd
-          return await get(request);
-        }
-      default:
-        throw Exception('invalid ${request.path}');
-    }
-  }
-
-  @override
-  Future<dynamic> get(Request request) async {
-    return db.get(id: request.path, fromJsonT: (v) => v);
   }
 }
 
