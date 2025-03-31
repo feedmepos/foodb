607193026efd73889e3451704fee192a1f165fc1
Lo King Wei <kingwei.lo@live.com.my>
2025-03-31 15:01:36 +0800

    feat: change objectbox interface into sync

--- foodb_objectbox_adapter/lib/foodb_objectbox_adapter.dart
+++ foodb_objectbox_adapter/lib/foodb_objectbox_adapter.dart
@@ -264,6 +264,13 @@ List<ObjectBoxType> allBoxes() => [
       allDocViewKeyMetaBox
     ];
 
+class ObjectBoxAdapterSession implements KeyValueAdapterSession {
+  @override
+  commit() {
+    throw UnimplementedError();
+  }
+}
+
 class ObjectBoxAdapter implements KeyValueAdapter {
   @override
   String type = 'object-box';
@@ -335,54 +342,53 @@ class ObjectBoxAdapter implements KeyValueAdapter {
   }
 
   @override
-  Future<bool> delete(AbstractKey<Comparable> key,
-      {KeyValueAdapterSession? session}) async {
+  bool delete(AbstractKey<Comparable> key, {KeyValueAdapterSession? session}) {
     final boxType = _getBoxFromKey(key);
-    final deleteResult = await boxType.remove(store, encodeKey(key));
+    final deleteResult = boxType.remove(store, encodeKey(key));
     return deleteResult == 1 ? true : false;
   }
 
   @override
-  Future<bool> deleteMany(List<AbstractKey<Comparable>> keys,
-      {KeyValueAdapterSession? session}) async {
+  bool deleteMany(List<AbstractKey<Comparable>> keys,
+      {KeyValueAdapterSession? session}) {
     for (final key in keys) {
-      await delete(key);
+      delete(key);
     }
     return true;
   }
 
   @override
-  Future<bool> clearTable(AbstractKey<Comparable> key,
-      {KeyValueAdapterSession? session}) async {
+  bool clearTable(AbstractKey<Comparable> key,
+      {KeyValueAdapterSession? session}) {
     final boxType = _getBoxFromKey(key);
     var encodedKey = encodeKey(key);
     if (key is ViewKeyMetaKey) {
       encodedKey = '${key.viewName}!';
     }
-    await boxType.removeAll(store, encodedKey);
+    boxType.removeAll(store, encodedKey);
     return true;
   }
 
   @override
-  Future<bool> destroy({KeyValueAdapterSession? session}) async {
-    await Future.wait(allBoxes().map((element) async {
+  bool destroy({KeyValueAdapterSession? session}) {
+    allBoxes().map((element) {
       return element.box(store).removeAll();
-    }));
+    });
     return true;
   }
 
   @override
-  Future<MapEntry<T, Map<String, dynamic>>?> get<T extends AbstractKey>(T key,
-      {KeyValueAdapterSession? session}) async {
+  MapEntry<T, Map<String, dynamic>>? get<T extends AbstractKey>(T key,
+      {KeyValueAdapterSession? session}) {
     final val = _getBoxFromKey(key).get(store, encodeKey(key));
     if (val == null) return null;
     return MapEntry(key, val.doc);
   }
 
   @override
-  Future<Map<T2, Map<String, dynamic>?>>
-      getMany<T2 extends AbstractKey<Comparable>>(List<T2> keys,
-          {KeyValueAdapterSession? session}) async {
+  Map<T2, Map<String, dynamic>?> getMany<T2 extends AbstractKey<Comparable>>(
+      List<T2> keys,
+      {KeyValueAdapterSession? session}) {
     if (keys.isEmpty) {
       return Map<T2, Map<String, dynamic>?>();
     }
@@ -401,26 +407,25 @@ class ObjectBoxAdapter implements KeyValueAdapter {
   }
 
   @override
-  Future<MapEntry<T2, Map<String, dynamic>>?>
-      last<T2 extends AbstractKey<Comparable>>(T2 key,
-          {KeyValueAdapterSession? session}) async {
+  MapEntry<T2, Map<String, dynamic>>? last<T2 extends AbstractKey<Comparable>>(
+      T2 key,
+      {KeyValueAdapterSession? session}) {
     final val = _getBoxFromKey(key).last(store, encodeKey(key));
     if (val == null) return null;
     return MapEntry(decodeKey(key, val.key) as T2, val.doc);
   }
 
   @override
-  Future<bool> put(AbstractKey<Comparable> key, Map<String, dynamic> value,
-      {KeyValueAdapterSession? session}) async {
-    await _getBoxFromKey(key).put(store, encodeKey(key), jsonEncode(value));
+  bool put(AbstractKey<Comparable> key, Map<String, dynamic> value,
+      {KeyValueAdapterSession? session}) {
+    _getBoxFromKey(key).put(store, encodeKey(key), jsonEncode(value));
     return true;
   }
 
   @override
-  Future<bool> putMany(
-      Map<AbstractKey<Comparable>, Map<String, dynamic>> entries,
-      {KeyValueAdapterSession? session}) async {
-    await _getBoxFromKey(entries.keys.first).putMany(
+  bool putMany(Map<AbstractKey<Comparable>, Map<String, dynamic>> entries,
+      {KeyValueAdapterSession? session}) {
+    _getBoxFromKey(entries.keys.first).putMany(
         store,
         entries
             .map((key, value) => MapEntry(encodeKey(key), jsonEncode(value))));
@@ -428,7 +433,7 @@ class ObjectBoxAdapter implements KeyValueAdapter {
   }
 
   @override
-  Future<ReadResult<T2>> read<T2 extends AbstractKey<Comparable>>(T2 keyType,
+  ReadResult<T2> read<T2 extends AbstractKey<Comparable>>(T2 keyType,
       {T2? startkey,
       T2? endkey,
       required bool desc,
@@ -436,7 +441,7 @@ class ObjectBoxAdapter implements KeyValueAdapter {
       required bool inclusiveStart,
       int? limit,
       int? skip,
-      KeyValueAdapterSession? session}) async {
+      KeyValueAdapterSession? session}) {
     final boxType = _getBoxFromKey(keyType);
     final totalRows = boxType.count(store);
     final offset = 0;
@@ -457,15 +462,14 @@ class ObjectBoxAdapter implements KeyValueAdapter {
   }
 
   @override
-  Future<void> runInSession(
-      Future<void> Function(KeyValueAdapterSession p1) function) {
-    // TODO: implement runInSession
-    throw UnimplementedError();
+  void runInSession(void Function(KeyValueAdapterSession p1) function) {
+    store.runInTransaction(
+        TxMode.write, () => function(ObjectBoxAdapterSession()));
   }
 
   @override
-  Future<int> tableSize(AbstractKey<Comparable> key,
-      {KeyValueAdapterSession? session}) async {
+  int tableSize(AbstractKey<Comparable> key,
+      {KeyValueAdapterSession? session}) {
     return _getBoxFromKey(key).count(store);
   }
 }
